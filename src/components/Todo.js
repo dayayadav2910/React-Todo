fc0bfbcf242304b237d1d903f21b18e4b55d24d4
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2022-02-15 21:05:33 +0530

    Routes added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,48 +1,52 @@
-import React, {useState} from 'react';
+import React from "react";
 import "./todo.css";
 
-const Todo = props =>{
+const Todo = (props) => {
+  let tasklists = props.tasklists;
+  let textValue = props.textValue;
+  let settasklists = props.settasklists;
+  let settextValue = props.settextValue;
 
-        const [textValue, settextValue] = useState('');
-        const [taskList, settaskList] = useState([])
-      
-        const handleClick = () => {
-         
-          if(textValue !== '')
-          {
-              const taskDetails = {
-                id : Math.floor(Math.random()*1000),
-                value:textValue,
-                iscompleted: false
-              }; 
-              settaskList([...taskList,taskDetails]);
-          } 
-        };
-        console.log(taskList)
-        const handleChange = (event) => {
-          settextValue(event.target.value);
-        }
+  const handleClick = () => {
+    if (textValue !== "") {
+      settasklists([...tasklists, textValue]);
+      settextValue("");
+    }
+  };
 
-        const deleteValue = (e,id) =>{
-            e.preventDefault();
-            settaskList(taskList.filter((t) => t.id != id));
-        }
-      
-        return (
-          <div className='todo'>
-            <input onChange={(e) => handleChange(e)} type="text" id="text"  placeholder="Type a message..." />
-            <button onClick={handleClick}  className='add-btn'> Click here</button>
-            {taskList!==[]?(
-                <ul>
-                    {taskList.map((t) =>(
-                       <li className='listitem'>{t.value}
-                       <button onClick={(e) => deleteValue(e,t.id)} className='delete'>Delete</button>
-                       </li>
-                       ))}
-                </ul>
-            ):null}
-          </div>
-        )
-    
-}
-export default Todo
+  const deleteValue = (id) => {
+    const updatedItems = tasklists.filter((elem, ind) => {
+      return ind !== id;
+    });
+    settasklists(updatedItems);
+  };
+
+  return (
+    <div className="todo">
+      <input
+        value={textValue}
+        onChange={(e) => settextValue(e.target.value)}
+        type="text"
+        id="text"
+        placeholder="Type a message..."
+      />
+      <button onClick={handleClick} className="add-btn">
+        {" "}
+        Click here
+      </button>
+      {tasklists !== [] ? (
+        <ul>
+          {tasklists.map((ele, ind) => (
+            <li className="listsitem" key={ind}>
+              {ele}
+              <button onClick={() => deleteValue(ind)} className="delete">
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+};
+export default Todo;
