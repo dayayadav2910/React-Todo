f6e9ad1451ffda0508afc3a2d918794f694afa1c
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2022-02-15 21:13:05 +0530

    file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import Todo from './components/Todo'
+import Navbar from './components/Navbar';
+import Todo from './components/Todo';
+import Tasklist from './components/Tasklist';
+import "./components/todo.css";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+const getItemsLocal = () =>{
+  let lists = localStorage.getItem('lists');
+  if(lists){
+    return JSON.parse(localStorage.getItem('lists'));
+  }
+  else{
+    return [];  
+  }
+}
 function App() {
+  
+  const [textValue, settextValue] = useState('');
+  const [tasklists, settasklists] = useState(getItemsLocal());
+  useEffect(() => {
+    localStorage.setItem('lists', JSON.stringify(tasklists))
+  }, [tasklists]);
   return(
-    <Todo></Todo>
-  )
+    <div>
+      <Router>
+       <Navbar></Navbar>
+       <Routes>
+          <Route exact path="/" element={<Todo textValue={textValue} settextValue={settextValue} tasklists={tasklists} settasklists={settasklists}  />}></Route>
+          <Route exact path="/tasklist" element={<Tasklist tasklists={tasklists} settasklists={settasklists}></Tasklist>}></Route>
+        </Routes>
+      </Router>
+    
+    </div>
+
+      )
 }
 
 export default App;
